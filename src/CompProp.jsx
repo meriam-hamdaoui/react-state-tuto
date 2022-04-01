43ceb45cc18ea9697f990609976d599fcc0826f7
meriam <hamdaoui.meriam85@gmail.com>
2022-04-01 10:46:16 +0200

    updates

---
+++ src/CompProp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class CompProp extends React.Component {
+  render() {
+    return (
+      <div className='CompProp'></div>
+    )
+  }
+}
+
+export default CompProp;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//we can also declare the html element in var
+// const element = <h1>Hello, world!</h1>;
+//we can add a jsx to our react by decalaring a variable
+//then add it to a string
+// const user ='Hamdaoui Mariem';
+//*ReactDOM.createRoot(document.getElementById('root')).render(<h1>Hello, {user}!</h1>);*/
+
+//we can add any type of valid JS expression to our jsx
+function fullName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName : 'Mariem',
+  lastName : 'Hamdaoui'
+}
+
+//const htmlEl = <h1>hello {fullName(user)}</h1>
+/*ReactDOM.createRoot(document.getElementById('root')).render(htmlEl);*/
+
+//we can use if statements or for loops inside our jsx code
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {fullName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+//specifying attributes with jsx
+//const el = <a href='https://www.reactjs.org'>link</a>
+//or use string literals
+//const element = <img src={user.avatarUrl}></img>;
+//jsx prevent injection attacks
+//const title = response.potentiallyMaliciousInput
+ReactDOM.createRoot(document.getElementById('root')).render(getGreeting(user));
+
+
+
+
 reportWebVitals();
